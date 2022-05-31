47f6b2355c52f6f568878c80d4e5a7c35a2c6e82
nfluk <nickylukic@gmail.com>
2022-05-31 20:30:39 +0200

    more promises practice

--- promises/slowCalc.js
+++ promises/slowCalc.js
@@ -1,16 +1,22 @@
-const p = new Promise((resolve, reject) => {
-  const a = 1;
-  if (a > 1) {
-    const myErr = new Error('Oups. Something went wrong.');
-    reject(myErr);
-  } else {
-    const message = 'Great. Everything looks fine.';
-    resolve(message);
-  }
-});
+const add = (num1, num2) => {
+  const p = new Promise((resolve, reject) => {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      const err = new Error('Please supply two numbers');
+      reject(err);
+    } else {
+      const sum = num1 + num2;
+      setTimeout(() => {
+        resolve(sum);
+      }, 2500);
+    }
+  });
 
-p.then((result) => {
-  console.log(result);
-}).catch((myErr) => {
-  console.log(myErr);
-});
+  p.then((result) => {
+    console.log(result);
+  }).catch((result) => {
+    console.log(result);
+  });
+};
+
+add(5, 4);
+add('hello', 5);
