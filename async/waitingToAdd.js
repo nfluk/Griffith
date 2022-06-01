355a62a639918ea957c41d072b9b317f60a9f18c
nfluk <nickylukic@gmail.com>
2022-06-01 22:17:36 +0200

    more practicing

--- async/waitingToAdd.js
+++ async/waitingToAdd.js
@@ -1,4 +1,17 @@
-const add = async (num1, num2) => {
+const promisifiedSetTimeout = (ms) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+const findErrors = (num1, num2) => {
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    const err = new Error('Please supply two numbers');
-}
+    throw new Error('Please supply two numbers');
+  }
+};
+
+const add = async (num1, num2) => {
+  findErrors(num1, num2);
+  const sum = num1 + num2;
+  await promisifiedSetTimeout(1000);
+  console.log(sum);
+};
+
+add(5, 11);
--- callbacks/punyAdd.js
+++ callbacks/punyAdd.js
@@ -1,9 +1,1 @@
-const add = (a, b, cb) => {
-  cb(a + b);
-};
 
-add(4, 5, (sum) => {
-  setTimeout(() => {
-    console.log(sum);
-  }, 2000);
-});
